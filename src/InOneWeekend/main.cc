d14053b9d8020034771b6332ae6da1d466594bc3
Varsha Singh <141292092+varshasing@users.noreply.github.com>
2025-04-07 14:23:35 -0400

    Update main.cc
    WIP: optmization + reasoning

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -25,6 +25,7 @@ int main() {
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
+    /* optimization: this is a small loop, but I wonder if loop unrolling would help. Don't really see a point of accumulators... */
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
